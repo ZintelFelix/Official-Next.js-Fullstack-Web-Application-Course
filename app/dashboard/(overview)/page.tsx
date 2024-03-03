5e48a3cb926ce9ce131e66bf20cdda2d79a59755
Felix Zintel <fezi1999@googlemail.com>
2024-03-03 01:07:03 +0100

    small change

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -27,8 +27,8 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton/>}>
-          <CardWrapper/>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
