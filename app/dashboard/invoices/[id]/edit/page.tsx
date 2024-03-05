f8c65094df457f4f0fd6c23e53ddf31416c45e3a
ZintelFelix <fezi1999@googlemail.com>
2024-03-05 11:45:12 +0100

    completed chapter 14

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -13,7 +13,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   if (!invoice) {
     notFound();
   }
-  
+
   return (
     <main>
       <Breadcrumbs
--- app/dashboard/invoices/error.tsx
+++ app/dashboard/invoices/error.tsx
@@ -1,7 +1,7 @@
 'use client';
- 
+
 import { useEffect } from 'react';
- 
+
 export default function Error({
   error,
   reset,
@@ -12,15 +12,13 @@ export default function Error({
   useEffect(() => {
     console.error(error);
   }, [error]);
- 
+
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h2 className="text-center">Something went wrong!</h2>
       <button
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={
-          () => reset()
-        }
+        onClick={() => reset()}
       >
         Try again
       </button>
